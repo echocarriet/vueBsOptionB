46f7969bf3edfad9a20dedd3bc9e9b3680954fc1
Carrie <echocarriet@gmail.com>
2022-08-09 18:26:26 +0800

    建立 v1.0

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,13 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <hr />
+  <div class="container">
+    <button class="btn btn-primary">按鈕</button>
+    <p class="fs-3 fs-md-1">
+      調整 → _variables.scss Line313 & _utilities.scss Line462
+    </p>
+  </div>
 </template>
 
 <script>
